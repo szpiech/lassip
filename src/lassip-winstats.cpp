06e536399a84e1c2a770b9f01e7e7234248041fa
Zachary Szpiech <szpiech@gmail.com>
2021-02-15 15:59:52 -0500

    Change the way we range across A

--- src/lassip-winstats.cpp
+++ src/lassip-winstats.cpp
@@ -189,7 +189,7 @@ void calcMTA(LASSIResults *results, double ****q, SpectrumData *specData, Spectr
 void calcMTA(LASSIResults *results, double ****q, SpectrumData *specData, SpectrumData *avgSpec, int w, double dmin){
    int MAX_EXTEND = 1000000; //bps each direction away from central window
    double minAlpha = 0.00001; //Smallest mixture proportion for Pr
-
+   double *dist = specData->dist;
    /*
    int rightLim, leftLim;
    double *dist = specData->dist;
